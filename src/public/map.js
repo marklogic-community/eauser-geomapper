7f9788f60352d7f827810c78c9a03e3eac0699d8
Danica Liang <dliang@marklogic.com>
2016-08-02 11:40:26 -0700

    new scrolling feature for selection tabs, added a date selector, and added bootstrap glyphicons

--- src/public/map.js
+++ src/public/map.js
@@ -360,3 +360,26 @@ function formatPopup(properties) {
 
   return str;
 }
+
+// $(function pickDate() {
+//   $('input[name="daterange"]').daterangepicker();
+// });
+
+$(function filterDate() {
+
+  $('input[name="datefilter"]').daterangepicker({
+      autoUpdateInput: false,
+      locale: {
+          cancelLabel: 'Clear'
+      }
+  });
+
+  $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+      $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
+  });
+
+  $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+      $(this).val('');
+  });
+
+});
