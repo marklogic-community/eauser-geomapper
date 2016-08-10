eca44f964534489c139c912fb668aa2a4615e219
Kevin Costello <kevincostello213@gmail.com>
2016-08-10 11:51:44 -0700

    Deleting unneeded method (teary eyed.. much time wasted writing it..) fixes #123 spider marker problem

--- src/public/map.js
+++ src/public/map.js
@@ -123,97 +123,9 @@ function addMapEvents() {
     doPost("/search.sjs", displayGeoJSON, false);
   });
   map.on('draw:deleted', function (e) {
-    // Update db to save latest changes.
-
-    // Loop through each deleted layer
-    /// (More than one could have been deleted)
-    for (var i in e.layers._layers) {
-      // Passing bounds of deleted drawn search regions
-      // Will remove all markers that exist only in the deleted regions
-      removeMarkers(e.layers._layers[i].getBounds());
-    }
-
-  });
-}
-
-// Check if markers are contained in bounds.
-// Remove all markers from map that are contained in bounds and not contained
-// in any drawn shapes on the map (if any);
-function removeMarkers(bounds) {
-  // loop through all markers on map
-  // and find if any are contained in bounds
-  // delete markers if they are contained in bounds
-  // and no other drawn shapes
-  var layers = drawnShapes.getLayers();
-  if (layers.length === 0) {
-    //if layers.length = 0 then no other drawn regions on map
-    // redraw markers that match search selections in this event
+    // Update db to save latest changes
     doPost("/search.sjs", displayGeoJSON, false);
-    return;
-  }
-
-
-  var markersObj;
-  for (var obj in markers._layers) {
-    // markersObj is an object of all marker objects currently on the map
-    // while there is only one object in markers._layers that has all
-    // map markers, it an id that changes every run of the map
-    // so using a loop to grab the name; ex: 163
-    // ** Same object in memory **
-    markersObj = markers._layers[obj]._layers;
-  }
-  // If markers on map, continue
-  // store markers here that shouldn't be deleted
-  var safeMarkers = [];
-  if (markersObj) {
-    for (var marker in markersObj) {
-      // looping through all map markers
-      // Check if the deleted drawn region (bounds) contains any markers
-      // on the map;
-
-      // LatLng object of marker to check if contained in the bounds of
-      // a deleted search region
-      var markerLatLng = markersObj[marker].getLatLng();
-      if (bounds.contains(markerLatLng)) {
-        // Before deleting, check if the marker is contained
-        // in other drawn regions. Don't delete marker if in
-        // other drawn region.
-        for (var layer in layers) {
-          if (layers[layer].getBounds().contains(markerLatLng)) {
-            // Mark as safe (not to remove) because this region
-            // contains the marker
-            // This drawn region is still on the map
-            // so don't remove marker from map
-            safeMarkers.push(marker);
-          }
-          else {
-            // Marker is not contained by current drawn layer
-            // so don't mark as safe
-          }
-        }
-
-      }
-      else { // if marker not contained by deleted shape,
-        // then don't delete from map
-
-        // Because there was a drawn region on the map
-        // before the delete, the only markers on the map should
-        // be those contained in a drawn search region on the map
-        // so assume this marker is within a different drawn region on map
-        // and mark it as safe
-        safeMarkers.push(marker);
-      }
-    }
-    // Delete all markers that weren't found in other drawn regions
-    for (var marker in markersObj) {
-      if (safeMarkers.indexOf(marker) === -1) {
-        // Marker isn't safe, must have only been found in th deleted
-        // region, so delete from map.
-        map.removeLayer(markersObj[marker]);
-      }
-    }
-  }
-  updateCount(safeMarkers);
+  });
 }
 
 // Draw markers on map
