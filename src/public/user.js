21bb0b1a18d83749cb848f6fa7e774d4118ce856
Dave Cassel <dmcassel@gmail.com>
2016-12-02 17:18:55 -0500

    rewrote the login/logout as a Vue component

--- src/public/user.js
+++ src/public/user.js
@@ -1,123 +1,91 @@
-/* global console */
+/* global console, Vue */
 
-// Builds HTML like this:
-// <div class="form-group">
-//   <label>label</label>
-//   <input name="name" type="type">
-// </div>
-function buildFormGroup(label, name, type) {
-  'use strict';
-
-  var group = document.createElement('div');
-  group.setAttribute('class', 'form-group');
-
-  var labelEl = document.createElement('label');
-  labelEl.appendChild(document.createTextNode(label));
-  group.appendChild(labelEl);
-
-  var input = document.createElement('input');
-  input.setAttribute('type', type);
-  input.setAttribute('name', name);
-  group.appendChild(input);
-
-  return group;
-}
-
-// Build the login form
-function buildLogin() {
-  'use strict';
-
-  var userDiv = document.getElementById('user');
-  userDiv.innerHTML = '';
-
-  var form = document.createElement('form');
-  form.setAttribute('class', 'form-inline');
-
-  form.appendChild(buildFormGroup('Username', 'username', 'text'));
-  form.appendChild(buildFormGroup('Password', 'password', 'password'));
-
-  var btn = document.createElement('button');
-  btn.setAttribute('class', 'btn btn-default btn-sm');
-  btn.appendChild(document.createTextNode('Login'));
-  btn.addEventListener('click', login, false);
-
-  form.appendChild(btn);
-  userDiv.appendChild(form);
-}
-
-function logout(event) {
-  'use strict';
-
-  $.ajax({
-    url: '/scripts/login/logout.sjs',
-    method: 'GET',
-    success: buildLogin,
-    failure: function() {
-      console.log('failed to log out');
-    }
-  });
-
-}
-
-// Display a welcome with the user's name
-function welcomeUser(username) {
-  'use strict';
-
-  var userDiv = document.getElementById('user');
-  userDiv.innerHTML = '';
-
-  var welcome = document.createElement('span');
-  welcome.setAttribute('class', 'welcome');
-  welcome.appendChild(document.createTextNode('Welcome, ' + username));
-
-  var logoutBtn = document.createElement('button');
-  logoutBtn.setAttribute('class', 'btn btn-default btn-sm');
-  logoutBtn.appendChild(document.createTextNode('Logout'));
-  logoutBtn.addEventListener('click', logout, false);
-
-  userDiv.appendChild(welcome);
-  userDiv.appendChild(logoutBtn);
-}
-
-function login(event) {
-  'use strict';
-
-  var username = document.querySelector('#user input[name=username').value;
-  var password = document.querySelector('#user input[name=password').value;
-
-  $.ajax({
-    url: '/scripts/login/login.sjs',
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    data: {
-      'username': username,
-      'password': password
-    },
-    dataType: 'json',
-    success: function(data, textStatus, jqXHR) {
-      welcomeUser(data.currentUser);
-    },
-    failure: function() {
-      console.log('login failed');
-    }
-  });
-
-  event.preventDefault();
-}
-
-$.ajax({
-  url: '/scripts/login/currentUser.sjs',
-  method: 'GET',
-  dataType: 'json',
-  success: function(data) {
+Vue.component('current-user', {
+  template:
+    '<div v-if="loggedIn">' +
+      '<span class="welcome">Welcome, {{username}}</span>' +
+      '<button class="btn btn-default btn-sm" v-on:click="logout">Logout</button>' +
+    '</div>' +
+    '<form v-else class="form-inline">' +
+      '<div class="form-group">' +
+        '<label>Username</label>' +
+        '<input v-model="username" type="text">' +
+      '</div>' +
+      '<div class="form-group">' +
+        '<label>Password</label>' +
+        '<input v-model="password" type="password">' +
+      '</div>' +
+      '<button class="btn btn-default btn-sm" v-on:click="login">Login</button>' +
+    '</form>',
+  data: function() {
     'use strict';
+    return {
+      username: '',
+      password: '',
+      loggedIn: false
+    };
+  },
+  created: function() {
+    'use strict';
+    console.log('created');
+    var vm = this;
 
-    if (data.isEditor) {
-      welcomeUser(data.currentUser);
-    } else {
-      buildLogin();
+    $.ajax({
+      url: '/scripts/login/currentUser.sjs',
+      method: 'GET',
+      dataType: 'json',
+      success: function(data) {
+
+        if (data.isEditor) {
+          vm.loggedIn = true;
+        } else {
+        }
+      }
+    });
+
+  },
+  methods: {
+    login: function(event) {
+      'use strict';
+
+      var vm = this;
+
+      $.ajax({
+        url: '/scripts/login/login.sjs',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: {
+          'username': this.username,
+          'password': this.password
+        },
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR) {
+          console.log('login success');
+          // vm.$set('loggedIn', true);
+          vm.loggedIn = true;
+        },
+        error: function() {
+          console.log('login failed');
+        }
+      });
+
+      event.preventDefault();
+    },
+    logout: function(event) {
+      'use strict';
+      var vm = this;
+      $.ajax({
+        url: '/scripts/login/logout.sjs',
+        method: 'GET',
+        success: function() {
+          vm.loggedIn = false;
+        },
+        error: function() {
+          console.log('failed to log out');
+        }
+      });
     }
   }
 });
