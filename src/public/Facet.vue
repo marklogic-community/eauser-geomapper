46f4191b609cd9fb016030578c64ab2098694991
Dave Cassel <dmcassel@gmail.com>
2016-12-06 12:18:53 -0500

    adding Facet.vue

---
+++ src/public/Facet.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="facet" id="">
+    <div class="">
+      <div class="panel-group" id="accordion">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <a data-toggle="collapse" v-bind:href="'#facet-' + this.id"><h4 class="panel-title">{{title}}</h4></a>
+          </div>
+          <div v-bind:id="'facet-' + id" class="panel-collapse collapse in">
+            <ul class="list-group" id="eaUL">
+              <li><label><input type="checkbox" class="select-all" id="select_all_i"/>Select All</label></li>
+              <!--<li v-for="item of items">
+                <label>
+                  <input type="checkbox" class="checker" value="item.value">
+                  <span>{{ item.message }}</span>
+                </label>
+              </li>-->
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    props: ['title'],
+    data: function() {
+      return {
+        id: -1
+      }
+    },
+    methods: {
+    },
+    created: function() {
+      this.id = this._uid
+    }
+  }
+</script>
+
+<style>
+</style>
